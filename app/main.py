10e5a49445a00fe913c89f5034b53ce2ad2b5828
Ivan Krupin <igelfiend@protonmail.com>
2023-03-20 15:12:57 +0100

    add root endpoint

---
+++ app/main.py
@@ -0,0 +1,29 @@
+import json
+
+
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+from pathlib import Path
+
+from app.base import Session
+from app.models import Employee
+from app.pydantic_models.employee import ResponseEmployee as PdResponseEmployee
+
+template_dir = Path(__file__).resolve().parent / "templates"
+
+
+@aiohttp_jinja2.template("index.html")
+async def root(request: web.Request):
+    with Session() as session, session.begin():
+        all_employees: list[Employee] = session.query(Employee).all()
+        empl_list = [PdResponseEmployee.from_orm(empl).dict() for empl in all_employees]
+        return {
+            "data": json.dumps(empl_list, default=str),
+        }
+
+
+app = web.Application()
+app.add_routes([web.get("/", root)])
+
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(str(template_dir)))
